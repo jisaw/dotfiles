91692ba8758f9975d55e9b332a7c5a52e7a2634b
Jake Sawyer <jsawyer@xpanxion.com>
2016-06-14 21:06:27 -0500

    updated setup.sh to make symlinks instead of copying

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo 'Distributing dotfiles\n'
 echo 'Moving .vimrc to ~/.vimrc\n'
-cp .vimrc ~/.vimrc
+ln -sf .vimrc ~/.vimrc
 echo 'Installing Vundle\n'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c :BundleInstall \
     -c qa
 echo 'Moving .zshrc to ~/.zshrc\n'
-cp .zshrc ~/.zshrc
+ln -sf .zshrc ~/.zshrc
 echo 'Moving .gitconfig to ~/.gitconfig\n'
-cp .gitconfig ~/.gitconfig
+ln -sf .gitconfig ~/.gitconfig
 echo 'Moving .tmux.conf to ~/.tmux.conf\n'
-cp .tmux.conf ~/.tmux.conf
+ln -sf .tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
 echo 'Finished\n'
