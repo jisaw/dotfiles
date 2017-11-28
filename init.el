a1ac3ac1e43a79087a3d5951016139fa9867bd0a
js28187 <sawyerjacob@johndeere.com>
2017-11-28 14:11:47 -0600

    udpated init.el

--- init.el
+++ init.el
@@ -1,9 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-tomorrow neotree which-key use-package try meghanada evil ensime))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'load-path "~/.emacs.d/neotree")
+(require 'neotree)
+  (global-set-key [f8] 'neotree-toggle)
+
 (setq inhibit-startup-message t)
 
 (require 'package)
-(setq package-enable-at-startup nil)
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
 (add-to-list 'package-archives
-             '["melpa" , "https://melpa.org/packages/"])
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -16,3 +44,11 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+(use-package evil
+  :ensure t
+  :config (evil-mode 1))
+
+(use-package scala-mode
+  :interpreter
+  ("scala" . scala-mode))
