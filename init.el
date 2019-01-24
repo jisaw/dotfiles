fd95451f175fe62dde5068567efe1e8cbee11546
Jacob Sawyer <SawyerJacob@JohnDeere.com>
2019-01-24 15:48:52 -0600

    added neotree to init.el

--- init.el
+++ init.el
@@ -39,6 +39,8 @@
 (menu-bar-mode -1)  
 (global-linum-mode)
 
+
+
 ;; global keybindings
 (global-unset-key (kbd "C-z"))
 
@@ -145,8 +147,13 @@ Repeated invocations toggle between the two most recently open buffers."
 
 (use-package magit
   :ensure t
-  :config (global-set-key (kbd "C-x g") 'magit-status)
-  )
+  :config (global-set-key (kbd "C-x g") 'magit-status))
+
+(use-package neotree
+  :ensure t
+  :config (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-smart-open t)
+  (setq projectile-switch-project-action 'neotree-projectile-action))
 
 (use-package undo-tree
   :config (global-undo-tree-mode))
@@ -714,7 +721,7 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
     ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "b35a14c7d94c1f411890d45edfb9dc1bd61c5becd5c326790b51df6ebf60f402" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "ec5f761d75345d1cf96d744c50cf7c928959f075acf3f2631742d5c9fe2153ad" "62c81ae32320ceff5228edceeaa6895c029cc8f43c8c98a023f91b5b339d633f" "f27c3fcfb19bf38892bc6e72d0046af7a1ded81f54435f9d4d09b3bff9c52fc1" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default)))
  '(package-selected-packages
    (quote
-    (doom-themes all-the-icons-dired doom-modeline darktooth darktooth-theme gruvbox-theme dracula-theme ample-theme powerline cider clojure-mode-extra-font-locking clojure-mode paredit smex popup-imenu highlight-symbol ido-completing-read+ markdown-mode ensime scala-mode auto-complete sbt-mode magit helm-projectile projectile which-key use-package undo-tree smartparens rainbow-mode rainbow-delimiters json-mode helm groovy-mode go-mode expand-region aggressive-indent))))
+    (neotree doom-themes all-the-icons-dired doom-modeline darktooth darktooth-theme gruvbox-theme dracula-theme ample-theme powerline cider clojure-mode-extra-font-locking clojure-mode paredit smex popup-imenu highlight-symbol ido-completing-read+ markdown-mode ensime scala-mode auto-complete sbt-mode magit helm-projectile projectile which-key use-package undo-tree smartparens rainbow-mode rainbow-delimiters json-mode helm groovy-mode go-mode expand-region aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
